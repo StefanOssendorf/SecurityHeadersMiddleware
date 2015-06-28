81f15bc63f523d1953102a3c4984539091dac93c
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2015-06-28 19:15:31 +0200

    Bumped to v 2.0.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Stefan Ossendorf 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]

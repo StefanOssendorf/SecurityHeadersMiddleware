3922ed63ecc3cd92439802bb5c733ee5662d95f3
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2016-11-09 10:26:49 +0100

    Ignore comment for Resharper added

--- src/SecurityHeaders.Owin.Examples/ContentTypeOptionsExamples.cs
+++ src/SecurityHeaders.Owin.Examples/ContentTypeOptionsExamples.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+// ReSharper disable ExpressionIsAlwaysNull
 
 namespace SecurityHeaders.Owin.Examples {
     using BuildFunc = Action<Func<IDictionary<string, object>, Func<Func<IDictionary<string, object>, Task>, Func<IDictionary<string, object>, Task>>>>;

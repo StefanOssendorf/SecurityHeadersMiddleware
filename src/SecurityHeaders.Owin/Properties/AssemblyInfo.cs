babd55c976629afdf30227913c041f11b5d34cdd
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2017-03-26 15:43:43 +0200

    Project settings adjusted
    shared.props and sharedIntegrationProj.props added for NuGet-Package
    values

--- src/SecurityHeaders.Owin/Properties/AssemblyInfo.cs
+++ src/SecurityHeaders.Owin/Properties/AssemblyInfo.cs
@@ -1,21 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SecurityHeaders.Owin")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e454c45-77bb-43a8-aa6e-8cbcde21dca6")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SecurityHeaders.Owin.Tests")]

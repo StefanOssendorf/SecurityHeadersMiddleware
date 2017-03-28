0b848e9a7beb28964636b42387b6cad73c5c2eb2
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2017-03-28 08:09:39 +0200

    ps1 modified to use NuGetV4
    Download url changed and path test removed

--- build.ps1
+++ build.ps1
@@ -91,7 +91,8 @@ $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
 #$MODULES_DIR = Join-Path $TOOLS_DIR "modules"
 $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
 $CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
-$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+#$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/v4.0.0/nuget.exe"
 $PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
 $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
 #$ADDINS_PACKAGES_CONFIG = Join-Path $ADDINS_DIR "packages.config"
@@ -132,25 +133,25 @@ if (!(Test-Path $PACKAGES_CONFIG)) {
 }
 
 # Try find NuGet.exe in path if not exists
-if (!(Test-Path $NUGET_EXE)) {
-    Write-Verbose -Message "Trying to find nuget.exe in PATH..."
-    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
-    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
-    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
-        Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-    }
-}
+# if (!(Test-Path $NUGET_EXE)) {
+    # Write-Verbose -Message "Trying to find nuget.exe in PATH..."
+    # $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
+    # $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
+    # if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
+        # Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
+        # $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
+    # }
+# }
 
 # Try download NuGet.exe if not exists
-if (!(Test-Path $NUGET_EXE)) {
+#if (!(Test-Path $NUGET_EXE)) {
     Write-Verbose -Message "Downloading NuGet.exe..."
     try {
         (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
     } catch {
         Throw "Could not download NuGet.exe."
     }
-}
+#}
 
 # Save nuget.exe path to environment to be available to child processed
 $ENV:NUGET_EXE = $NUGET_EXE

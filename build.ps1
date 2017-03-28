538604b89e52e6f5e40920b4824b73a88e484686
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2017-03-28 17:49:42 +0200

    build.ps1 cleaned up

--- build.ps1
+++ build.ps1
@@ -91,12 +91,9 @@ $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
 #$MODULES_DIR = Join-Path $TOOLS_DIR "modules"
 $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
 $CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
-#$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $NUGET_URL = "https://dist.nuget.org/win-x86-commandline/v4.0.0/nuget.exe"
 $PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
 $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
-#$ADDINS_PACKAGES_CONFIG = Join-Path $ADDINS_DIR "packages.config"
-#$MODULES_PACKAGES_CONFIG = Join-Path $MODULES_DIR "packages.config"
 
 # Should we use mono?
 $UseMono = "";
@@ -132,30 +129,15 @@ if (!(Test-Path $PACKAGES_CONFIG)) {
     }
 }
 
-# Try find NuGet.exe in path if not exists
-# if (!(Test-Path $NUGET_EXE)) {
-    # Write-Verbose -Message "Trying to find nuget.exe in PATH..."
-    # $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
-    # $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
-    # if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
-        # Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-        # $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-    # }
-# }
-
-# Try download NuGet.exe if not exists
-#if (!(Test-Path $NUGET_EXE)) {
-    Write-Verbose -Message "Downloading NuGet.exe..."
-    try {
-        (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
-    } catch {
-        Throw "Could not download NuGet.exe."
-    }
-#}
+Write-Verbose -Message "Downloading NuGet.exe..."
+try {
+	(New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
+} catch {
+	Throw "Could not download NuGet.exe."
+}
 
 # Save nuget.exe path to environment to be available to child processed
 $ENV:NUGET_EXE = $NUGET_EXE
-#$ENV:Path = "$($NUGET_EXE);$($ENV:Path)"
 
 # Restore tools from NuGet?
 if(-Not $SkipToolPackageRestore.IsPresent) {

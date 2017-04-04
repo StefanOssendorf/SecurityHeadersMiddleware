f73cddd979652b9c33f1dc3c035b7dc61eeb6520
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2017-04-04 14:29:28 +0200

    Refactored
    Missed to save...

--- src/SecurityHeaders/XFrameOptionHeaderValue.cs
+++ src/SecurityHeaders/XFrameOptionHeaderValue.cs
@@ -79,7 +79,7 @@ namespace SecurityHeaders {
                 return true;
             }
 
-            if (obj.GetType() != this.GetType()) {
+            if (obj.GetType() != GetType()) {
                 return false;
             }
 
